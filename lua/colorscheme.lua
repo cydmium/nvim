d1ea907b6301b09a68aecfeadec13586e7987692
David Richardson <davidrichardson96@gmail.com>
2021-05-22 20:58:40 -0400

    Update colorscheme for alacritty

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,1 +1,8 @@
+local utils = require'utils'
+if vim.fn.has("termguicolors") == 1 then
+  utils.opt('o', 'termguicolors', true)
+end
+vim.g.gruvbox_material_enable_bold = true
+vim.g.gruvbox_material_enable_italic = true
+vim.g.gruvbox_material_palette = 'mix'
 vim.cmd([[colorscheme gruvbox-material]])
